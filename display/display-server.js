0885ca8db882aed888589ab339b0d39d0b49cf5f
Ryan H <6423385+rhampt@users.noreply.github.com>
2022-09-04 11:15:15 -0400

    Restructure 2
    Change-type: patch
    Signed-off-by: Ryan Hampton (@rhampt) <rhampt@balena.io>

---
+++ display/display-server.js
@@ -0,0 +1,22 @@
+const { join } = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const app = express();
+const PORT = 82;
+
+app.use(favicon(join(__dirname, 'views', 'public', 'favicon.ico')));
+app.set('views', join(__dirname, 'views', 'public'));
+app.set('view engine', 'html');
+
+// Enable the public directory for resource files
+app.use('/public', express.static(join(__dirname, 'views', 'public')));
+
+// reply to request with the hello world html file
+app.get('/', function (req, res) {
+  res.sendFile(join(__dirname, 'views', 'index.html'));
+});
+
+// start a server on port 82 and log its start to our console
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
